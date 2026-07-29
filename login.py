958f21c055020f3fc37eb3b5c8dfa2a27db1feb4
neilrewal1-commits <neilrewal1@gmail.com>
2026-07-28 20:50:31 -0700

    Update login.py

--- login.py
+++ login.py
@@ -3,7 +3,7 @@ imageaddress = 'images.jpg'
 st.title('Predicting Stomach Adenocarcinoma TMB Status')
 
 st.image(
-    image_address,
+    imageaddress,
     caption="Stomach Adenocarcinoma TMB Classification",
 )
 
