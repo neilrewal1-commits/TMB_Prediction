02c01190e1e3f3b673233c3e9b6d1f690c978c08
neilrewal1-commits <neilrewal1@gmail.com>
2026-07-30 20:51:30 -0700

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,17 +1,22 @@
 import streamlit as st
-imageaddress = 'images.jpg'
-st.title('Predicting Stomach Adenocarcinoma TMB Status')
 
+IMAGE_ADDRESS = ("https://scmsc.com/wp-content/uploads/2024/12/3D-illustration-of-stomach-cancer.jpg")
+
+# Home page
+st.title("Stomach Adenocarcinoma TMB Classification")
+
+# Add a coral reef image
 st.image(
-    imageaddress,
+    IMAGE_ADDRESS,
     caption="Stomach Adenocarcinoma TMB Classification",
 )
+
 
 if not st.user.is_logged_in:
     if st.sidebar.button("Log in with Google", type="primary", icon=":material/login:"):
         st.login()
 
 else:
-  if st.sidebar.button("Log out", type="secondary", icon=":material/logout:"):
+    if st.sidebar.button("Log out", type="secondary", icon=":material/logout:"):
         st.logout()
         st.stop()
